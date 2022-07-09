fff34d61ee1634454bd79809dc4706ea90f3bf0f
yohan-lanier <72730254+yohan-lanier@users.noreply.github.com>
2022-07-09 17:25:12 +0200

    def a main function. Code is more interactive and displays computation progresses

--- main.py
+++ main.py
@@ -8,21 +8,40 @@ def plot_points(points):
     plt.plot(points[:,0], points[:,1], '+-')
     plt.show()
 
+def main():
+    if len(sys.argv) == 3:
+        file = sys.argv[-2]
+        N = int(sys.argv[-1])
+    elif len(sys.argv) == 2 : 
+        N = 10
+        file = sys.argv[-1]
+    else : 
+        print('invalid number of arguments')
+
+    if file.split('.')[-1] == 'svg':
+        path_list = extract_paths_from_svg_file(file)
+        n_paths = len(path_list)
+        print(f'{n_paths} svg paths detected, only the first one will be processed \n')
+        print('chose number of points for the discretization of the svg path. Please enter an integer number. 500 is a good basis\n')
+        n_points = int(input())
+        points = read_svg_path_and_return_XY_tab(path_list[0], n_points=500)
+        #format the list of points from svg coordinate system to matplotlib coordinate system
+        points = format_list_of_points(points)
+        #plot the detected path for pre-visualization
+        plot_points(points)
+        fig_lims = compute_fig_lims(points)
+        Time = np.linspace(0, 1, points.shape[0])# as I have taken the convention of defining the function between 0 and 1 in the fourier notations
+        X = points[:,0]
+        Y = points[:,1]
+        #Compute the cn coefficient of the Fourier serie
+        cn = compute_cn(Time, X, Y, N)
+        #Compute the serie coefficient for all time t in table T
+        print('\nComputing serie coefficients for all times t\n')
+        SerieCoef = np.array([compute_fourier_serie_terms(cn, t, N) for t in tqdm(Time)]).T
+        #Create the animation
+        anim = visualize(SerieCoef, N, fig_lims)
+        plt.show()
+
 if __name__ == '__main__':
-
-    filename = sys.argv[-1]
-    N = 40
-    path_list = extract_paths_from_svg_file(filename)
-    points = read_svg_path_and_return_XY_tab(path_list[0], n_points=500)
-    plot_points(points)
-
-    points = format_list_of_points(points)
-    fig_lims = compute_fig_lims(points)
-    Time = np.linspace(0, 1, points.shape[0])# as I have taken the convention of defining the function between 0 and 1 in the fourier notations
-    X = points[:,0]
-    Y = points[:,1]
-    cn = compute_cn(Time, X, Y, N)
-    SerieCoef = np.array([compute_fourier_serie_terms(cn, t, N) for t in Time]).T
-    anim = visualize(SerieCoef, N, fig_lims)
-    plt.show()
+    main()
 
